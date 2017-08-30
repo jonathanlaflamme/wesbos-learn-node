c5c27402dc7891668bda5fb024f787d028ed5349
Jonathan Laflamme <jonathan.laflamme.jl@gmail.com>
2017-08-30 18:09:33 -0400

    finished multiple videos. Now on logout

--- models/Store.js
+++ models/Store.js
@@ -13,17 +13,50 @@ const storeSchema = new mongoose.Schema({
         type: String,
         trim: true,
     },
-    tags: [String]
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: 'You must supply coordinates!'
+        }],
+        address: {
+            type: String,
+            required: 'You must supply an address!'
+        }
+    },
+    photo: String
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
     if(!this.isModified('name')) {
         next();
         return;
     }
     this.slug = slug(this.name);
+    // find other slug with same name, name-1, name-2
+    const slugRegex = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storeWithSlug = await this.constructor.find({slug: slugRegex});
+    if(storeWithSlug.length) {
+        this.slug = `${this.slug}-${storeWithSlug.length + 1}`
+    }
     next();
     // TODO: make more work
 })
+
+storeSchema.statics.getTagsList = function() {
+    return this.aggregate([
+        { $unwind: '$tags' },
+        { $group: { _id: '$tags', count: { $sum: 1 } } },
+        { $sort: { count: -1} }
+    ]);
+}
 
 module.exports = mongoose.model('Store', storeSchema);
